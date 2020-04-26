0c7febed9838ccc140f0afc6baeccb03541923b4
Alexander Akulich <akulichalexander@gmail.com>
2020-04-26 18:22:55 +0300

    PresetsLoader: Refactor built-in presets

--- example/PresetsLoader.cpp
+++ example/PresetsLoader.cpp
@@ -2,11 +2,21 @@
 
 QList<ManagerPreset> PresetsLoader::presets()
 {
-    static QList<ManagerPreset> presets = {
-        ManagerPreset("simplecm", "simplecm"),
-        ManagerPreset("ofono", "ofono"),
-        ManagerPreset("ring", "tel"),
-    };
+    static QList<ManagerPreset> presets;
+    if (presets.isEmpty()) {
+        {
+            ManagerPreset preset("simplecm", "simplecm");
+            presets << preset;
+        }
+        {
+            ManagerPreset preset("ofono", "ofono");
+            presets << preset;
+        }
+        {
+            ManagerPreset preset("ring", "tel");
+            presets << preset;
+        }
+    }
 
     return presets;
 }
